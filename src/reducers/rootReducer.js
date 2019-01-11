b5de1d6c822e92849ab1b4978da59e6148d78ca1
hzjqn <hzdes.95@gmail.com>
2019-01-11 13:09:45 -0300

    corrected rootReducer to use a switch instead of conditionals

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -5,61 +5,65 @@ let initialState = {
     convertTo: "ARS",
     amount: 0,
     result: 0
-}   
+}
+
+const SWAP_CURRENCIES = "SWAP_CURRENCIES";
+const UPDATE_RATES = "UPDATE_RATES";
+const UPDATE_BASE_CURRENCY = "UPDATE_BASE_CURRENCY";
+const UPDATE_CONVERSION_CURRENCY = "UPDATE_CONVERSION_CURRENCY";
+const UPDATE_AMOUNT = "UPDATE_AMOUNT";
+const UPDATE_CONVERSION_RESULT = "UPDATE_CONVERSION_RESULT";
 
 const rootReducer = (state, action) => {
     if (state === undefined){
         state = initialState
     }
 
-    if(action.type === "SWAP_CURRENCIES"){
-        return {
-            ...state,
-            baseCurrency: state.convertTo,
-            convertTo: state.baseCurrency,
-            amount: state.result,
-            result: state.amount
-        }
-    }
+    switch (action.type){
+        case SWAP_CURRENCIES:
+            return {
+                ...state,
+                baseCurrency: state.convertTo,
+                convertTo: state.baseCurrency,
+                amount: state.result,
+                result: state.amount
+            }
 
-    if(action.type === "UPDATE_RATES"){
-        return {
-            ...state,
-            fetched: true,
-            rates: action.rates
-        }
-    }
+        case UPDATE_RATES:
+            return {
+                ...state,
+                fetched: true,
+                rates: action.rates
+            }
 
-    if(action.type === "UPDATE_BASE_CURRENCY"){
-        return {
-            ...state,
-            baseCurrency: action.currency
-        }
-    }
-    
+        case UPDATE_BASE_CURRENCY:
+            return {
+                ...state,
+                baseCurrency: action.currency
+            }
 
-    if(action.type === "UPDATE_CONVERSION_CURRENCY"){
-        return {
-            ...state,
-            convertTo: action.currency
-        }
-    }
+        case UPDATE_CONVERSION_CURRENCY:
+            return {
+                ... state,
+                convertTo: action.currency
+            }
 
-    if(action.type === "UPDATE_AMOUNT"){
-        return {
-            ...state,
-            amount: action.amount
-        }
-    }
-    
+        case UPDATE_AMOUNT:
+            return {
+                ... state,
+                amount: action.amount
+            }
 
-    if(action.type === "UPDATE_CONVERSION_RESULT"){
-        return {
-            ...state,
-            result: action.result
+        case UPDATE_CONVERSION_RESULT:
+            return {
+                ... state, 
+                result: action.result
+            }
+        
+        default: {
+            return state
         }
     }
-    return state;
 }
 
 export default rootReducer;
